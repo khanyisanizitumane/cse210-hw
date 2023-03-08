6610ddf9633c13c28a7a8e0670ee991036bda5c8
khanyisanizitumane <zitumanekhanyisani@gmail.com>
2023-03-08 13:34:53 +0200

    week 2

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        DisplayWelcomeMessage();
+        String name = Console.ReadLine();
+
+       
+        
+        
+
+        
     }
+   
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Journal Program\nPlease select one of the following choices. \n1.Write \n2.Display \n3.Load \n4.Save \n5.Quit");
+       
+    }
+
+    
 }
